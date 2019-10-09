4311be109629898c653710b5f4a91a37f076bcaf
TimGroenenberg <tim@connectholland.nl>
2019-10-09 15:53:15 +0200

    Configure at-rule-empty-line-before to ignore import

--- index.js
+++ index.js
@@ -3,6 +3,20 @@
 module.exports = {
     "extends": "stylelint-config-standard",
     "rules": {
+        "at-rule-empty-line-before": [
+            "always", {
+                "except": [
+                    "blockless-after-same-name-blockless",
+                    "first-nested",
+                ],
+                "ignore": [
+                    "after-comment"
+                ],
+                "ignoreAtRules": [
+                    "import"
+                ]
+            }
+        ],
         "at-rule-no-unknown": [
             true,
             {
